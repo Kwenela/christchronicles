36384346c7a61e3d37835a42e3b6c1a4080f51e4
Kwenela <137670121+Kwenela@users.noreply.github.com>
2023-09-22 22:42:59 -0700

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://servant-status-ministries.notion.site/Christ-Chronicles-a462d76973dd44e5bb2453b9dc97b95d?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
